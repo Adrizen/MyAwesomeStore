14fe8dbecbc28ec070152e0f00dcd50a8cb89cab
Adrizen <36826976+Adrizen@users.noreply.github.com>
2023-01-18 13:21:38 -0300

    Agregar archivos de proyecto.

---
+++ AdminPresentationLayer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdminPresentationLayer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

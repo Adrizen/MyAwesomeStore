1a2d3e46fb4d5acd4329ff334f8ed39689cbcbc6
Adrizen <36826976+Adrizen@users.noreply.github.com>
2023-01-20 11:59:17 -0300

    Added create new brand functionality

--- AdminPresentationLayer/Controllers/BrandController.cs
+++ AdminPresentationLayer/Controllers/BrandController.cs
@@ -1,12 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AdminPresentationLayer.Data;
+using AdminPresentationLayer.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AdminPresentationLayer.Controllers
 {
     public class BrandController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public BrandController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            IEnumerable<Brand> brandList = _db.Brand;
+
+            return View(brandList);
+        }
+
+        // GET
+        public IActionResult Create()
+        {
+            
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Brand brand)
+        {
+            _db.Brand.Add(brand);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }

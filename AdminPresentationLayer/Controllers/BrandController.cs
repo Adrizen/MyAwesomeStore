5e10eae7cf99d4f9d4d8f629a8d359bbc6c3b64d
Adrizen <36826976+Adrizen@users.noreply.github.com>
2023-01-21 13:28:08 -0300

    Added full brand functionality

--- AdminPresentationLayer/Controllers/BrandController.cs
+++ AdminPresentationLayer/Controllers/BrandController.cs
@@ -31,9 +31,85 @@ namespace AdminPresentationLayer.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Brand brand)
         {
-            _db.Brand.Add(brand);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                _db.Brand.Add(brand);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            } else
+            {
+                return View(brand);
+            }
+
+            
         }
+
+        // Edit
+        public IActionResult Edit(int? idBrand)
+        {
+            if (idBrand == null || idBrand == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Brand.Find(idBrand);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Brand brand)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Brand.Update(brand);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(brand);
+            }
+        }
+
+        // Delete
+        public IActionResult Delete(int? idBrand)
+        {
+            if (idBrand == null || idBrand == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Brand.Find(idBrand);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Brand brand)
+        {
+            if (brand == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _db.Brand.Remove(brand);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+
     }
 }
